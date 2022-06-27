c1c8275072ca823ef0d4224d0e258d4a174da397
CorentinSolal <corentin.solal2022@campus-eni.fr>
2022-06-27 14:22:09 +0200

    final

--- src/main/java/fr/eni/projectsevenenchere/bo/User.java
+++ src/main/java/fr/eni/projectsevenenchere/bo/User.java
@@ -2,16 +2,13 @@ package fr.eni.projectsevenenchere.bo;
 
 public class User {
 
-
-    private String adresse;
-    private int cpo;
-    private String email;
-    private int telephone;
-
-
     private Integer noUser;
     private String pseudo;
     private String nom;
     private String Prenom;
+    private String adresse;
+    private int cpo;
+    private String email;
+    private int telephone;
 
 }
