43fd792f003d27643b9978cf606fa31d6bc41af8
arthurdauplay <arthur.dauplay2022@campus-eni.fr>
2022-06-28 14:46:30 +0200

    ArticleDAO

---
+++ src/main/java/fr/eni/projectsevenenchere/ihm/ArticleFactory.java
@@ -0,0 +1,10 @@
+package fr.eni.projectsevenenchere.ihm;
+
+import fr.eni.projectsevenenchere.bll.ArticleManager;
+
+public class ArticleFactory {
+
+    public static ArticleManager getArticleManager() {
+     return new ArticleManagerImpl();
+    }
+}
