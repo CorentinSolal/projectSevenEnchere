92048c92d7dbdc2695597900c52192559f090a6a
Tsiory144 <tsiory.ranaivoharison2022@campus-eni.fr>
2022-06-28 11:52:34 +0200

    article (sans DAO)

--- src/main/java/fr/eni/projectsevenenchere/bll/ArticleManager.java
+++ src/main/java/fr/eni/projectsevenenchere/bll/ArticleManager.java
@@ -5,8 +5,7 @@ import fr.eni.projectsevenenchere.bo.Article;
 import java.util.List;
 
 public interface ArticleManager {
-
-    List<Article> getAllArticles throws BLLException;
+    List<Article> getAllArticles() throws BLLException;
     void enregistrerArticle(Article article) throws BLLException;
-    Article getArticleById(Int id) throws BLLException;
+
 }
