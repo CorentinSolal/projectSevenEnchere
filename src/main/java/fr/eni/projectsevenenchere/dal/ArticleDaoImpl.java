2cdb2058e948de992bfe08abc6aeb1b19b7f4440
arthurdauplay <arthur.dauplay2022@campus-eni.fr>
2022-06-28 16:13:19 +0200

    Update ArticleDaoImpl.java

--- src/main/java/fr/eni/projectsevenenchere/dal/ArticleDaoImpl.java
+++ src/main/java/fr/eni/projectsevenenchere/dal/ArticleDaoImpl.java
@@ -7,14 +7,16 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ArticleDaoImpl   {
+public class ArticleDaoImpl implements ArticleDao  {
 
     private static final String INSERT = "insert into ARTICLE (nom_article, description, date_debut_encheres, date_fin_encheres, prix_initial, prix_vente) values(?,?,?,?,?,?)";
     private static final String SELECT_ALL ="select * from ARTICLE";
 
-    private static final String SELECT_BY_ID = "select * from ARTICLE where no_article=? ";
+   // private static final String SELECT_BY_ID = "select * from ARTICLE where no_article=? ";
 
     private static final String DELETE = "delete from ARTICLE where no_article = ?";
+
+    private static final String UPDATE = "update ARTICLE set nom=?, description=?, date_debut_encheres=?, date_fin_encheres=?, prix_initial=?, prix_vente=?" + "where no-article=?";
 
 
     public void insert(Article article) throws DALException {
@@ -62,7 +64,7 @@ public class ArticleDaoImpl   {
 
             Article articleAjout = null;
             while (rs.next()) {
-                // utilisation du conscruteur Pizza id,nom,description,image,prix
+                // utilisation du conscruteur Article
                 articleAjout = new Article(rs.getInt("no_article"),rs.getString("nom_article"),rs.getString("description"), (rs.getDate("date_debut_encheres")).toLocalDate(),(rs.getDate("date_fin_encheres")).toLocalDate(),rs.getInt("prix_initial"),rs.getInt("prix_vente"));
                 listeArticle.add(articleAjout);
             }
@@ -76,7 +78,7 @@ public class ArticleDaoImpl   {
 
     }
 
-    @Override
+   /* @Override
     public Article selectById(Integer id) throws DALException {
         Article article = null;
         // ouverture et fermeture de la connection
@@ -103,6 +105,62 @@ public class ArticleDaoImpl   {
         }
 
         return article;
+
+    }*/
+
+    @Override
+    public void delete(Integer id) throws DALException {
+
+        try ( // Try with resources
+              Connection conn = ConnectionProvider.getConnection();) {
+
+            // Statement stmt = conn.createStatement();
+            PreparedStatement stmt = conn.prepareStatement(DELETE);
+
+            // TODO : faire l'insert
+            // Valoriser les paramètres
+            stmt.setInt(1, id);
+
+            int nbRows = stmt.executeUpdate();
+
+            if (nbRows == 0) {
+                throw new DALException("erreur delete - id not found : " + id);
+            }
+
+        } catch (SQLException e) {
+
+            throw new DALException("erreur delete article : " + id, e);
+        }
+
+    }
+
+    @Override
+    public void update(Article article) throws DALException {
+        try ( // Try with resources
+              Connection conn = ConnectionProvider.getConnection();) {
+
+            // Statement stmt = conn.createStatement();
+            PreparedStatement stmt = conn.prepareStatement(UPDATE);
+
+            // TODO : faire l'insert
+            // Valoriser les paramètres
+            stmt.setString(1, article.getNomArt());
+            stmt.setString(2, article.getDescArt());
+            stmt.setDate(3, Date.valueOf(article.getDateDebut()));
+            stmt.setDate(4, Date.valueOf(article.getDateFin()));
+            stmt.setInt(5, article.getPrixInit());
+            stmt.setInt(6, article.getPrixFinal());
+
+            int nbRows = stmt.executeUpdate();
+
+            if (nbRows == 0) {
+                throw new DALException("erreur update - id not found : " + article.getIdArt());
+            }
+
+        } catch (SQLException e) {
+
+            throw new DALException("erreur update pizza : " + article.getIdArt(), e);
+        }
 
     }
 }
