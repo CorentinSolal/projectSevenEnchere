9f75f80f9f8ec884524246163a9b4cecbb5f1309
CorentinSolal <corentin.solal2022@campus-eni.fr>
2022-06-28 14:24:06 +0200

    trad ANG

--- src/main/java/fr/eni/projectsevenenchere/ihm/ArticleServlet.java
+++ src/main/java/fr/eni/projectsevenenchere/ihm/ArticleServlet.java
@@ -21,12 +21,12 @@ public class ArticleServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-        request.getRequestDispatcher("/WEB-INF/pages/Home.jsp").forward(request, response);
+        request.getRequestDispatcher("/WEB-INF/pages/home.jsp").forward(request, response);
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-        request.getRequestDispatcher("/WEB-INF/pages/Home.jsp").forward(request, response);
+        request.getRequestDispatcher("/WEB-INF/pages/home.jsp").forward(request, response);
     }
 }
