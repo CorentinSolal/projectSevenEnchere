276d2596a5dc78894dab51e60f92192e575fb8c9
Tsiory144 <tsiory.ranaivoharison2022@campus-eni.fr>
2022-06-30 11:14:07 +0200

    tsiory 11:13

--- src/main/java/fr/eni/projectsevenenchere/dal/RetraitDAOImpl.java
+++ src/main/java/fr/eni/projectsevenenchere/dal/RetraitDAOImpl.java
@@ -6,13 +6,28 @@ import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import static sun.tools.jconsole.Messages.CREATE;
 
 public class RetraitDAOImpl implements RetraitDAO{
     // TODO FAIRE LA DAO POUR LIER LA BO A LA BASE DE DONNEES
 
+    public void addRetrait(Retrait retrait) throws DALException {
+        try (Connection conn = ConnectionProvider.getConnection();) {
 
+            PreparedStatement stmt = conn.prepareStatement(CREATE, PreparedStatement.RETURN_GENERATED_KEYS);
+
+            stmt.executeUpdate();
+
+            ResultSet rs = stmt.getGeneratedKeys();
+            if(rs.next()) {
+               // retrait.setNoArt(rs.getInt(1));
+            }
+        } catch (SQLException e) {
+            throw new DALException("error create article : ", e);
+        }
+    }
 
 
 
